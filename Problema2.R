8a8f4fc9352e284514102411773946a7bca62be6
eloi.gil.fernandez <eloi.gil.fernandez@auleseebe.upc.edu>
2025-03-04 09:24:42 +0100

    ...

--- Problema2.R
+++ Problema2.R
@@ -1,1 +1,39 @@
 #Codigo para problema 2
+mis_dades <- iris
+mis_dades
+x<-mis_dades$Petal.Length
+mean(x)#media
+sd(x)#distania típica
+#histograma
+hist(x)
+
+y <- mis_dades$Sepal.Length
+mean(y)
+#grafico x,y
+plot(x,y)
+#sacar m
+m<- sum((x-mean(x))*(y-mean(y)))/sum((x-mean(x))^2)
+m
+#sacar b
+b<- mean(y)-m*mean(x)
+b
+#prediccion
+m*1.5+b
+####
+mod<-lm(y~x)
+mod
+summary(mod)
+#predicciones
+data.frame(x=1.5)
+predict(mod, data.frame(x=1.5))
+
+data.frame(x=x)
+ypred<-predict(mod, data.frame(x=x))
+#grafico
+plot(x,y, col="red",pch=16)
+lines(x,ypred)
+#recta r
+Rsq<- sum((ypred-mean(y))^2)/sum((y-mean(y))^2)
+Rsq
+
+summary(mod)
